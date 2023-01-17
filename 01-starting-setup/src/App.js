a69a217709fbf44fca8665ce48ab12999e2098f7
Paulina Filipiak <slodzinska.paulina@gmail.com>
2023-01-17 21:51:55 +0100

    added ExpenseItem component

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,9 +1,13 @@
+import ExpenseItem from "./components/ExoenseItem/ExpenseItem"
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
-  );
+  )
 }
+s
 
-export default App;
+export default App
