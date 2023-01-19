551485a2a758db46b09dc421f9d72f03b079929a
Paulina Filipiak <slodzinska.paulina@gmail.com>
2023-01-19 14:37:15 +0100

    added ExpenseForm and NewExpense components

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,5 +1,5 @@
 import ExpensesAll from "./components/Expenses/ExpensesAll/ExpensesAll"
-
+import NewExpense from "./components/NewExpense/NewExpense"
 function App() {
   const expenses = [
     {
@@ -24,7 +24,7 @@ function App() {
   ]
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <ExpensesAll expenses={expenses} />
     </div>
   )
---
+++ 01-starting-setup/src/components/NewExpense/NewExpense.js
@@ -0,0 +1,11 @@
+import "./newExpense.css"
+import ExpenseForm from "./ExpenseForm/ExpenseForm"
+
+const NewExpense = () => {
+  return (
+    <div className="new-expense">
+      <ExpenseForm />
+    </div>
+  )
+}
+export default NewExpense
