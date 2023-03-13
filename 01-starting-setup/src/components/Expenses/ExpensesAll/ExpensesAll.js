d3343b158b7ba0a7744b2025a66b13be681879ea
Paulina Filipiak <slodzinska.paulina@gmail.com>
2023-03-13 21:36:36 +0100

    used two-way-binding to pass the data from child element to parent- filterInput

--- 01-starting-setup/src/components/Expenses/ExpensesAll/ExpensesAll.js
+++ 01-starting-setup/src/components/Expenses/ExpensesAll/ExpensesAll.js
@@ -2,31 +2,39 @@ import ExpenseItem from "../ExpenseItem/ExpenseItem"
 import Card from "../../UI/Card/Card"
 import "../../UI/Card/card.css"
 import "./ExpensesAll.css"
+import ExpensesFilter from "../ExpensesFilter/ExpensesFilter"
+import { useState } from "react"
 function ExpensesAll(props) {
-  return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      />
-      <ExpenseItem
-        title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
-        date={props.expenses[3].date}
-      />
-    </Card>
-  )
+	const [inputValue, setinputValue] = useState("2020")
+	const onFilterInput = (queryValue) => {
+		setinputValue(queryValue)
+	}
+	console.log(inputValue)
+	return (
+		<Card className="expenses">
+			<ExpensesFilter selected={inputValue} onFilterInput={onFilterInput} />
+			<ExpenseItem
+				title={props.expenses[0].title}
+				amount={props.expenses[0].amount}
+				date={props.expenses[0].date}
+			/>
+			<ExpenseItem
+				title={props.expenses[1].title}
+				amount={props.expenses[1].amount}
+				date={props.expenses[1].date}
+			/>
+			<ExpenseItem
+				title={props.expenses[2].title}
+				amount={props.expenses[2].amount}
+				date={props.expenses[2].date}
+			/>
+			<ExpenseItem
+				title={props.expenses[3].title}
+				amount={props.expenses[3].amount}
+				date={props.expenses[3].date}
+			/>
+		</Card>
+	)
 }
 
 export default ExpensesAll
