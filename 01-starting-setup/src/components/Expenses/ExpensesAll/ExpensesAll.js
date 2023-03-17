00a53de65c026ca3d6becee814b85a798575e8f5
Paulina Filipiak <slodzinska.paulina@gmail.com>
2023-03-17 15:26:13 +0100

    added filter by year

--- 01-starting-setup/src/components/Expenses/ExpensesAll/ExpensesAll.js
+++ 01-starting-setup/src/components/Expenses/ExpensesAll/ExpensesAll.js
@@ -6,29 +6,39 @@ import ExpensesFilter from "../ExpensesFilter/ExpensesFilter"
 import { useState } from "react"
 
 function ExpensesAll(props) {
-	const [inputValue, setinputValue] = useState("2020")
+	const [inputValue, setinputValue] = useState("")
 	// const [filteredData, setFilteredData] = useState(props.items)
 
 	const onFilterInput = (queryValue) => {
 		setinputValue(queryValue)
-		// const filtered = props.items.filter(
-		// 	(expense) => expense.date.getFullYear().toString() === inputValue
-		// )
-		// setFilteredData(filtered)
 	}
+	const filtered = props.items.filter(
+		(expense) => expense.date.getFullYear().toString() === inputValue
+	)
 	console.log(inputValue)
 	return (
 		<Card className="expenses">
 			<ExpensesFilter selected={inputValue} onFilterInput={onFilterInput} />
 			{/* {filteredData.length === 0 && <p>No expenses found.</p>} */}
-			{props.items.map((item) => (
-				<ExpenseItem
-					key={item.id}
-					title={item.title}
-					amount={item.amount}
-					date={item.date}
-				/>
-			))}
+			{inputValue === "" &&
+				props.items.map((item) => (
+					<ExpenseItem
+						key={item.id}
+						title={item.title}
+						amount={item.amount}
+						date={item.date}
+					/>
+				))}
+			{filtered.length > 0 &&
+				filtered.map((item) => (
+					<ExpenseItem
+						key={item.id}
+						title={item.title}
+						amount={item.amount}
+						date={item.date}
+					/>
+				))}
+			{filtered.length === 0 && <p>No such items!</p>}
 		</Card>
 	)
 }
